7c001bc81a7673b0269dd181d0cf8655ad683b9c
mypigmylove <yxxb@163.com>
2018-01-18 15:59:09 +0800

    负载均衡器项目

---
+++ 设计模式征途/01单例模式/Models/LoaderBalence.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01单例模式.Models
+{
+    class LoaderBalence
+    {
+        private static LoaderBalence _instance = null;
+
+        private List<CustomServer> _customServerList;
+
+
+        private LoaderBalence()
+        {
+            _customServerList=new List<CustomServer>();
+        }
+
+
+        public static LoaderBalence GetInstance()
+        {
+            if (_instance==null)
+            {
+                _instance=new LoaderBalence();
+            }
+
+            return _instance;
+        }
+
+
+        public void AddCustom(CustomServer cs)
+        {
+            this._customServerList.Add(cs);
+        }
+
+        public void RemoveCustome(CustomServer cs)
+        {
+
+            _customServerList.Remove(_customServerList.Find(server => server.Name.Equals(cs.Name)));
+        }
+
+
+        private Random _random=new Random();
+
+        public CustomServer GetServer()
+        {
+            int index = _random.Next(_customServerList.Count);
+            return _customServerList[index];
+        }
+
+    }
+}
--- 设计模式征途/01单例模式/Program.cs
+++ 设计模式征途/01单例模式/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _01单例模式.Models;
 
 namespace _01单例模式
 {
@@ -10,6 +11,16 @@ namespace _01单例模式
     {
         static void Main(string[] args)
         {
+            LoaderBalence lb1=LoaderBalence.GetInstance();
+            LoaderBalence lb2=LoaderBalence.GetInstance();
+            LoaderBalence lb3 = LoaderBalence.GetInstance();
+
+            if (lb1.Equals(lb2))
+            {
+                Console.WriteLine("相同的实例");
+            }
+
+            Console.ReadKey();
         }
     }
 }
