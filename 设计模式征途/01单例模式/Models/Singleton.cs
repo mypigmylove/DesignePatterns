eae86e151597f4469c4428c21a2ac6c1a962b00d
mypigmylove <yxxb@163.com>
2018-01-18 15:40:20 +0800

    单例类

---
+++ 设计模式征途/01单例模式/Models/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01单例模式.Models
+{
+    class Singleton
+    {
+
+        private static Singleton _singleton;
+
+        public static Singleton GetSingleton()
+        {
+            if (_singleton==null)
+            {
+                _singleton=new Singleton();
+            }
+
+            return _singleton;
+        }
+
+
+
+        private Singleton()
+        {
+            
+        }
+    }
+}
